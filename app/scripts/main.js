ace808c47349d0df4c7191061df7f744ce17ef41
Hasan Khan <has_03@hotmail.com>
2015-12-11 16:34:59 +0000

    applying the hover over animation and styling for the image grid very simple and quick jQuery functionality for the hover over applying a landscape mixin which deals with mobile landscape and tablet range took out things i didn't need from animate.css and imported into the main.scss file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+'use strict';
+
+function imgBox (){
+	var $imgBox = $('.img-box');
+	var $imgHover = $('.img-box__hover');
+	var $imgContent = $('.img-box__content');
+
+	$imgBox.click(function(e){
+		e.preventDefult();
+	});
+
+	$imgBox.stop().hover(function() {
+		var $this = $(this);
+		$this.find('.img-box__hover').removeClass("fadeOutLeftBig").addClass("fadeInLeft");
+		$this.find('.img-box__content').addClass('img-box__scaleIt');
+	}, function() {
+		var $this = $(this);
+		$this.find('.img-box__hover').removeClass("fadeInLeft").addClass("fadeOutLeftBig");
+		$this.find('.img-box__content').removeClass('img-box__scaleIt');
+	});	
+};
+
